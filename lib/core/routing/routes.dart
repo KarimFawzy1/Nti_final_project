dc8baf6b3d532d976a281835ac6170d84ab41c83
Karim Fawzy <karim.fawzy180@gmail.com>
2025-12-23 22:33:49 +0200

    Updated Colors

--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -1,4 +1,3 @@
-class Routes{
+class Routes {
   static const String chatsConversation = "/chatsConversation";
-  
 }
---
+++ lib/features/chats/chats_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ChatsScreen extends StatelessWidget {
+  const ChatsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.chat), label: 'Chats'),
+          BottomNavigationBarItem(icon: Icon(Icons.group), label: 'Groups'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list_rounded),
+            label: 'More',
+          ),
+        ],
+      ),
+      body: Column(children: [Text('Chats Screen')]),
+    );
+  }
+}
